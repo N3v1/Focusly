6f21f01386c53e433a0f934bafa9bf26b2cd0a3a
N3v1 <n3v1010@gmail.com>
2024-12-13 16:55:37 +0100

    Initial Commit

---
+++ FocuslyTests/FocuslyTests.swift
@@ -0,0 +1,17 @@
+//
+//  FocuslyTests.swift
+//  FocuslyTests
+//
+//  Created by Nevio Hirani on 13.12.24.
+//
+
+import Testing
+@testable import Focusly
+
+struct FocuslyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
